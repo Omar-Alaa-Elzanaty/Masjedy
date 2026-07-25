270d207bb5324ce07fb8ff9d180fdb2ddba3f2b2
omar alaa <omaralaaelzanaty@gmail.com>
2026-07-25 22:30:29 +0300

    Add mapping annotations to MosqueMapper interface

--- src/main/java/com/masjedy/Application/Mapper/MosqueMapper.java
+++ src/main/java/com/masjedy/Application/Mapper/MosqueMapper.java
@@ -1,6 +1,7 @@
 package com.masjedy.Application.Mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 import com.masjedy.Application.Dtos.MosqueDto;
@@ -8,5 +9,9 @@ import com.masjedy.Domain.Models.Mosque;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MosqueMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "users", ignore = true)
     Mosque toEntity(MosqueDto.CreateRequest request);
 }
